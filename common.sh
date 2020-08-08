f48474ab75798ea20d943b29d983a4e8a638bd21
henloboi <henloboii@protonmail.ch>
2020-08-08 12:08:31 +0800

    non dynamic workarounds

---
+++ common.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+aria2c http://tdrive.manofuranium.workers.dev/ROS-MIUI_RedmiNote9S_20.7.29_v12-10.zip
+unzip *.zip -d work
+rm -f *.zip
+cd work
+rm -f system.new.dat.br system.patch.dat system.transfer.list
+cd ..
+aria2c $BASE
+unzip *.zip system.new.dat.br system.transfer.list system.patch.dat product.new.dat.br product.transfer.list product.patch.dat -d work -qq
+rm -f *.zip
+cd work
+zip -r $ZIPNAME_RedmiNote9S_$MIUIVER_henlotools-10.zip
