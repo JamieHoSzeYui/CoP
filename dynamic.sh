642db6c9a811b091400cd55d34f13b24ddf4634f
henloboi <henloboii@protonmail.ch>
2020-08-08 12:09:02 +0800

    dynamic workarounds

--- dynamic.sh
+++ dynamic.sh
@@ -2,7 +2,9 @@
 aria2c http://tdrive.manofuranium.workers.dev/xiaomi.eu_multi_RedmiNote9Pro_RedmiNote9S_20.7.30_v12-10.zip
 unzip *.zip -d work
 rm -f *.zip
+cd work
 rm -rf system.new.dat.br system.patch.dat system.transfer.list product.new.dat.br product.transfer.list product.patch.dat
+cd ..
 aria2c $BASE
 unzip *.zip system.new.dat.br system.transfer.list system.patch.dat product.new.dat.br product.transfer.list product.patch.dat -d work -qq
 rm -f *.zip
